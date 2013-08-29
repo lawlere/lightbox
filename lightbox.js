44b3ac10a135bb9230fd3d131132077847adabc3
lawlere <emily.lawler@colorado.edu>
2013-08-28 21:32:13 -0600

    lightbox trial

---
+++ lightbox.js
@@ -0,0 +1,11 @@
+function showLightbox()
+{
+	document.getElementById("lightboxBG").style.display = "block";
+	document.getElementById("lightboxFG").style.display = "block";
+}
+
+function hideLightbox()
+{
+	document.getElementById("lightboxBG").style.display = "none";
+	document.getElementById("lightboxFG").style.display = "none";
+}
